813b3acefbb3b9971a787ca594e64e9966ed1e50
Melissa Hale <melissakayh@gmail.com>
2024-08-08 21:42:09 -0500

    remove api call to delete the service (#1)
    Signed-off-by: Melissa Hale <melissakayh@gmail.com>

--- initService/initiate-replica-set.sh
+++ initService/initiate-replica-set.sh
@@ -56,25 +56,11 @@ check_all_nodes "${nodes[@]}"
 
 echo "All MongoDB nodes are up. Initiating replica set..."
 
-# Initiate replica set and capture result
-if ! initiate_replica_set; then
+# Initiate replica set, fail if it doesn't complete successfully
+if initiate_replica_set; then
+  echo "Replica set initiated successfully. Exiting script..."
+  exit 0
+else
   echo "Failed to initiate replica set. Please check the logs for more information."
   exit 1
 fi
-
-# Execute GraphQL mutation to remove the init service
-echo "Executing GraphQL mutation to remove the init service..."
-response=$(curl --silent --location "$RAILWAY_API_URL" \
-  --header 'Content-Type: application/json' \
-  --header "Authorization: Bearer $RAILWAY_API_TOKEN" \
-  --data "{\"query\":\"mutation serviceDelete(\$environmentId: String, \$id: String!) { serviceDelete(environmentId: \$environmentId, id: \$id) }\",\"variables\":{\"environmentId\":\"$ENVIRONMENT_ID\",\"id\":\"$SERVICE_ID\"}}")
-
-# Check for errors in the GraphQL response
-if echo "$response" | grep -q '"errors"'; then
-  echo "Failed to delete the service via the API. Response: $response"
-  exit 1
-else
-  echo "GraphQL mutation executed successfully. Response: $response"
-fi
-
-exit 0
